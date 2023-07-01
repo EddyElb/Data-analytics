6b48134acc9ec4ac750ef0c8df7c7ea4af299539
EddyElb <138258656+EddyElb@users.noreply.github.com>
2023-07-01 18:51:32 +0200

    Create un_declearion-hr_insights.py
    Python data Structure

---
+++ data_analytics/natural_language_processing/projects/text mining/un_declearion-hr_insights.py
@@ -0,0 +1,11 @@
+file_path = "path/to/your/file.txt"
+file = open(r"C:\Users\Ediya Mwaluka Amour\Downloads\un_declaration_hr_text_data.txt")
+
+# Read the contents of the file
+file_contents = file.read()
+
+# Close the file
+file.close()
+
+# Print the contents of the file
+print(file_contents)
